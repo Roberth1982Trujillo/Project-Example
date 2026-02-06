10a241cc31275988aaff1aa360a96e5b3e345845
unknown <SUITE@SUITEDH114-SERV.cibertec.edu.pe>
2026-02-06 11:14:51 -0500

    mi primer commit

---
+++ src/main/java/cibertec/pe/service/AlumnoImplement.java
@@ -0,0 +1,38 @@
+package cibertec.pe.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cibertec.pe.model.Alumno;
+import cibertec.pe.repository.IAlumnoRepository;
+
+@Service
+public class AlumnoImplement implements IAlumnoService {
+
+	@Autowired
+	private IAlumnoRepository alumnoRepo;
+	
+	@Override
+	public List<Alumno> listarAlumnos() {
+		return alumnoRepo.findAll();
+	}
+
+	@Override
+	public Alumno guardarAlumno(Alumno alumno) {		
+		return alumnoRepo.save(alumno);
+	}
+
+	@Override
+	public Optional<Alumno> buscarAlumno(int codigo) {
+		return alumnoRepo.findById(codigo);
+	}
+
+	@Override
+	public void eliminarAlumno(int codigo) {
+		alumnoRepo.deleteById(codigo);
+	}
+
+}
